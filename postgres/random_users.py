298caa4cf4e0134028a05bd64913e6ceea4ea969
Khadem <a.khadem@digikala.com>
2023-05-31 02:23:03 +0330

    chore: clean up random_users.py file

--- postgres/random_users.py
+++ postgres/random_users.py
@@ -1,24 +1,28 @@
 import random
 import names
-random.uniform(0, 1)
+
+TABLE_NAME = "User"
+FIELD_NAMES = ("name", "family", "age", "sex")
+SEXES = (0, 1)
+MIN_AGE = 20
+MAX_AGE = 80
+NUM_USERS = 300
+
 with open("insert_users.sql", "w") as f:
-    line_1 = 'INSERT INTO "User" (name, family, age, sex) VALUES\n'
-    f.write(line_1)
+    first_line = 'INSERT INTO "{}" ({}) VALUES\n'.format(TABLE_NAME, ", ".join(FIELD_NAMES))
+    f.write(first_line)
+
     value_sql = "('{name}', '{family}', {age}, {sex})"
-    length = 300
-    for i in range(length):
-        """
-        male = 0
-        female = 1
-        """
-        sex = 0 if random.uniform(0, 1) > 0.5 else 1
+    for i in range(NUM_USERS):
+        sex = random.choice(SEXES)
         name = names.get_first_name()
         family = names.get_last_name()
-        age = random.randrange(20, 80)
-        f.write(value_sql.format(name=name, age=age, family=family, sex=sex))
-        if i != length - 1:
-            f.write(",\n")
-    end_exec = ";"
-    f.write(end_exec)
-    f.flush()
+        age = random.randint(MIN_AGE, MAX_AGE)
+        
+        values = value_sql.format(name=name, age=age, family=family, sex=sex)
+        if i == NUM_USERS - 1:
+            end_exec = ";"
+            values += end_exec
+        f.write(values + "\n")
 
+    f.flush()
